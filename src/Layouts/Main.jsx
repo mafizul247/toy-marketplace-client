decbad3d9b6980dab9901e5fe8360de7af3fb34d
mafizul247 <mafizul247@gmail.com>
2023-05-18 22:22:06 +0600

    Footer Add

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navber from '../components/Shared/Navber';
+import Footer from '../components/Shared/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navber/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
