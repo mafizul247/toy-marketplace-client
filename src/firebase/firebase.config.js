900b114454ddd3e6b2f33d3277300e9bd304224d
mafizul247 <mafizul247@gmail.com>
2023-05-19 11:26:21 +0600

    Firebase Local Env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCivzgv_HLcud0Eaf-480cMslDqv_ZQZug",
-  authDomain: "toy-marketplace-39472.firebaseapp.com",
-  projectId: "toy-marketplace-39472",
-  storageBucket: "toy-marketplace-39472.appspot.com",
-  messagingSenderId: "868140513066",
-  appId: "1:868140513066:web:66865f6de3c020838dd053"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
